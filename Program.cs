c049126cf8be8a84755831f1c0b49f5c066c090b
LWooler <lwooler@att.net>
2021-05-17 14:52:00 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Strategy_Checkpoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
